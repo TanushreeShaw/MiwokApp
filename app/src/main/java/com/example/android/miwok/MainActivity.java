896ebacbd4d0455d18856503b28683b8a8a0b220
Tanushree Shaw <63643049+TanushreeShaw@users.noreply.github.com>
2021-02-10 15:04:41 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/android/miwok/MainActivity.java
+++ app/src/main/java/com/example/android/miwok/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.android.miwok;
+
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
+
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 
